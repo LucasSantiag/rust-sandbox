05616a994b55491459ade3906191820973a8be4a
LucasSantiag <lucassantiago17.cardoso08@gmail.com>
2020-04-29 10:20:12 -0300

    Types in rust

---
+++ src/types.rs
@@ -0,0 +1,13 @@
+pub fn run() {
+    let x = 1; //default -> i32
+
+    let y = 2.5; //default -> f64
+
+    let z: i64 = 231423132;
+
+    println!("x = {}, y = {}, z = {}", x, y, z);
+
+    println!("Max i32: {}", std::i32::MAX);
+
+    //i32 vs u32 -> u.OnlyPositiveNumber
+}
